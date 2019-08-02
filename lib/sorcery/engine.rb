ebfd8cc232fc3a549e256ae5ec39e316dc473ee3
Kiyotaka Sasaya <kiyotaka.sasaya@medpeer.co.jp>
2019-08-02 19:56:56 +0900

    Add some hack to make sorcery work

--- lib/sorcery/engine.rb
+++ lib/sorcery/engine.rb
@@ -6,5 +6,10 @@ module Sorcery
   # With the plugin logic.
   class Engine < Rails::Engine
     config.sorcery = ::Sorcery::Controller::Config
+
+    initializer 'extend something with sorcery' do
+      # NOTE: This will trigger sorcery configuration
+      Module.new.include Sorcery::Controller
+    end
   end
 end
